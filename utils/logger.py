29c9f519abe151a6e5756dd70d15e38a2ad67d97
W-Andres <86696243+W-Andres@users.noreply.github.com>
2026-06-28 19:43:05 -0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,21 @@
+import logging
+import os
+
+class LoggerManager:
+    """
+    Administrador centralizado de registros para el sistema SAT-ML SCM.
+    """
+    @staticmethod
+    def get_logger(name=__name__):
+        logger = logging.getLogger(name)
+        if not logger.handlers:
+            logger.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            
+            # Consola
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(formatter)
+            logger.addHandler(console_handler)
+            
+        return logger
 
